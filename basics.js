71070a6a0a36ba977cb146ebcd7a0c18f241b474
Abhishek Kumar Yadav <yadavabhishek5869@gmail.com>
2022-05-27 21:04:52 +0530

    First Commit This is my first commit.

---
+++ basics.js
@@ -0,0 +1,19 @@
+// Using a variable to store string and number 
+
+var fruit = "apple";
+var price = 100 ;
+
+var el = document.getElementById('box1');
+el.textContent = fruit;
+
+console.log(el.textContent)
+
+var el2 = document.getElementById('secondbox1');
+ 
+el2.textContent = price;
+
+// Using quotes inside a string 
+
+var message = ''
+
+var el3 = document.getElementById('box1');
